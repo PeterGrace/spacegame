793f3d7efd8884917497d4a8efb48fccc91b6b85
Nextraztus <relay@nextraztus.com>
2015-07-03 14:22:44 -0600

    initial stab at socket server

---
+++ spacegame/Server/Server.py
@@ -0,0 +1,104 @@
+'''Telnet-related server stuffs for connectivity.'''
+
+import select
+import socket
+import sys
+import threading
+import logging
+
+
+class MUDServer(object):
+    def __init__(self, logger=None, *args, **kwargs):
+        self._host = kwargs.get('host', '0.0.0.0')
+        self._port = kwargs.get('port', 5280)
+        self._backlog = kwargs.get('backlog', 5)
+
+        self._threads = []
+        self._size = 1024
+        self._server = None
+        self._threads = []
+
+        self.logger = logging.getLogger('main.%s' % (self.__class__.__name__,))
+
+    def _bind_socket(self):
+        try:
+            self._server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self._server.bind((self._host, self._port))
+            self._server.listen(self._backlog)
+        except socket.error, (value, message):
+            if self._server:
+                self._server.close()
+            self.logger.critical("Could not open server socket: %s", message)
+            sys.exit(1)
+
+    def run(self):
+        self._bind_socket()
+        input_fds = [self._server, sys.stdin]
+        running = 1
+        while running:
+            inputready, outputready, exceptready = select.select(input_fds, [], [])
+
+            for s in inputready:
+                if s == self._server:
+                    # accept connection and spawn off player thread
+                    clientid = len(self._threads) + 1
+
+                    c = Client(self._server.accept(), clientid)
+                    self.logger.info('Client %d connected: %s:%s', clientid,
+                                     c.address[0], c.address[1])
+                    c.start()
+                    self._threads.append(c)
+
+                elif s == sys.stdin:
+                    # TODO: console commands
+                    junk = sys.stdin.readline()
+                    running = 0
+
+            # thread cleanup
+            for c in self._threads:
+                if not c.isAlive():
+                    # TODO: future results (if any) needed from client cleanup
+                    c.cleanup = True
+
+            self._threads = [t for t in self._threads if not t.cleanup]
+
+        # close all threads
+        self._server.close()
+        for c in self._threads:
+            c.join()
+
+
+class Client(threading.Thread):
+    def __init__(self, (client, address), clientid):
+        threading.Thread.__init__(self)
+        self.client = client
+        self.address = address
+        self.size = 1024
+        self.playerid = None
+        self.clientid = clientid
+        self.running = True
+        self.cleanup = False
+
+        self.logger = logging.getLogger('main.%s.%d' % (self.__class__.__name__,
+                                                        self.clientid))
+
+    def join(self, timeout=None):
+        self.client.send("Server is terminating.\n")
+        self.running = False
+        super(Client, self).join(timeout)
+
+    def run(self):
+        self.running = True
+        while self.running:
+            try:
+                data = self.client.recv(self.size)
+            except IOError:
+                self.logger.info('Client %d disconnected.', self.clientid)
+                self.running = False
+                continue
+
+            if data:
+                self.client.send(data)
+            else:
+                self.client.close()
+                running = False
--- spacegame/main.py
+++ spacegame/main.py
@@ -1,11 +1,30 @@
 """The primary module for this skeleton application."""
 import click
+import logging
+import sys
+from spacegame.Server import Server
 
 
 @click.command()
 def main():
     '''Skeleton App made by pymkcli'''
-    print 'Hello, I am spacegame!'
+
+    # TODO: get configuration from command line
+
+    # TODO: read log level from configuration
+    logger = logging.getLogger('main')
+    logger.setLevel(logging.DEBUG)
+    log_hdlr = logging.StreamHandler(sys.stdout)
+    log_hdlr.setFormatter(logging.Formatter('%(asctime)s %(levelname)-8s %(message)s'))
+    logger.addHandler(log_hdlr)
+
+    # TODO: database?
+    # TODO: bring up Universe
+
+    print "Enter any input to quit."
+    server = Server.MUDServer(port=5281)
+    server.run()
+
 
 if __name__ == '__main__':
     main()
