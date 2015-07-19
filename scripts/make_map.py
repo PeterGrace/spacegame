3ed97f61002792f1fbc8e95a065884f5bd535fac
Peter Grace <pete.grace@gmail.com>
2015-07-19 21:26:09 +0000

    make a better behaved bigbang

--- scripts/make_map.py
+++ scripts/make_map.py
@@ -2,43 +2,67 @@
 import random
 import sys
 import pickle
+import pdb
 from spacegame.Universe import Universe, Sector
+import code, traceback, signal
+
+def debug(sig, frame):
+    """Interrupt running process, and provide a python prompt for
+    interactive debugging."""
+    d={'_frame':frame}         # Allow access to frame object.
+    d.update(frame.f_globals)  # Unless shadowed by global
+    d.update(frame.f_locals)
+
+    i = code.InteractiveConsole(d)
+    message  = "Signal received : entering python shell.\nTraceback:\n"
+    message += ''.join(traceback.format_stack(frame))
+    i.interact(message)
+
+def listen():
+    signal.signal(signal.SIGUSR1, debug)  # Register handler
 
 
-def make_new_sector(backlink,newsector,level=1):
-    if u.get_sector(newsector) is None:
-        sector=Sector.Sector(sector_name=newsector)
-        sector.add_link(backlink)
-        u.add_sector(sector)
-        size=len(u.map.keys())
-        print("Added sector {sector}.  Universe now {size} sectors big.".format(sector=sector.name,size=size))
-        numlinks=random.randint(1,MAX_LINKS-1)
-        for new_links in range(1,numlinks):
-            if len(u.map.keys()) == 1000:
-                print "We're done here. Level={level}".format(level=level)
-                return True
-            newnewsector=random.randint(1,UNIVERSE_SIZE)
-            created=False
-            while not created:
-               created=make_new_sector(newsector, newnewsector,level+1)
-               newnewsector=random.randint(1,UNIVERSE_SIZE)
-        pickle.dump( u, open( "universe.p", "wb" ) )
-        return True
-    else:
-        return False           
+def bang_sector():
+    '''make a new sector and put it in the universe.'''
+    newsector=random.randint(1,UNIVERSE_SIZE)
+    while u.get_sector(newsector) is not None:
+        newsector=random.randint(1,UNIVERSE_SIZE) 
+        if len(u.map.keys()) >= UNIVERSE_SIZE:
+            return None
+
+    s=Sector.Sector(sector_name=newsector)
+    u.add_sector(s)
+    return newsector
+
+def link_sectors(previous,current):
+    sector_A=u.map[previous]
+    sector_B=u.map[current]
+
+    sector_A.add_link(sector_B)
+    sector_B.add_link(sector_A)
 
 
-
-UNIVERSE_SIZE=1000
+UNIVERSE_SIZE=100
 MAX_LINKS=6
 
-sys.setrecursionlimit(UNIVERSE_SIZE+1)
-
+listen()
 u=Universe.Universe()
 
-center=random.randint(1,UNIVERSE_SIZE)
-s=Sector.Sector(sector_name=center)
-u.add_sector(s)
-next_to_center=random.randint(1,UNIVERSE_SIZE)
-make_new_sector(center,next_to_center)
+prev_sector_id=bang_sector()
 
+while len(u.map.keys()) <= UNIVERSE_SIZE:
+    sector_id=bang_sector()
+    if sector_id is None:
+        break
+    link_sectors(prev_sector_id,sector_id)
+    numlinks=random.randint(1,MAX_LINKS)
+    size=len(u.map.keys())
+    print("Added sector {sector}.  Universe now {size} sectors big.".format(sector=sector_id,size=size))
+    for j in range(1,numlinks):
+        link_sector_id=bang_sector()
+        if link_sector_id is None:
+            break
+        link_sectors(sector_id, link_sector_id)
+        size=len(u.map.keys())
+        print("Added adjacency count {j} of sector {s} in {sector}.  Universe now {size} sectors big.".format(sector=sector_id,j=j,s=link_sector_id,size=size))
+pickle.dump( u, open( "universe.p", "wb" ) )
